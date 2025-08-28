f0c4b8ad0b30edb97de2021cafb2ae28488d1b30
Isaac Baker <ibaker2029@kehillahstudent.org>
2025-08-28 09:04:09 -0700

    found some errors

---
+++ hello_world.py
@@ -0,0 +1,41 @@
+print()
+
+
+
+#  File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1, in <module>
+#    ergge
+#NameError: name 'ergge' is not defined
+
+#  File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 2, in <module>
+#    print(gsdgf)
+#          ^^^^^
+#NameError: name 'gsdgf' is not defined
+
+# File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1
+#    print(g"dgf)
+#           ^
+#SyntaxError: unterminated string literal (detected at line 1)
+
+#  File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1, in <module>
+#    prit(gdgf)
+#    ^^^^
+#NameError: name 'prit' is not defined. Did you mean: 'print'?
+
+# File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1, in <module>
+#    tyj
+#NameError: name 'tyj' is not defined
+
+#  File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1
+#    if("ggsdf")
+#               ^
+#SyntaxError: expected ':'
+
+#File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1
+#    if:print("heeheeheehaw")
+#      ^
+#SyntaxError: invalid syntax
+
+# File "/Users/isaactbaker/github-classroom/tkscs/01-hello-world-and-error-hunt-SackOfBlueSquares/hello_world.py", line 1
+#    if:
+#      ^
+#SyntaxError: invalid syntax
